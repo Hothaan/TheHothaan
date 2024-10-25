8a1a4e9d7174e28edd81ed51beba32130a45bc7a
han songyi <hhhsi92@naver.com>
2024-10-25 19:34:18 +0900

    [MOD] swagger url 변경

--- server/swagger.js
+++ server/swagger.js
@@ -23,7 +23,7 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwagger = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 module.exports = setupSwagger;
