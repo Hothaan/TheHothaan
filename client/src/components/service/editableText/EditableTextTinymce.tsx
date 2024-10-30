8867e83c260f7a11024d2e392a9c1147760854f5
imlinkseo <imlinkseo@gmail.com>
2024-10-30 14:57:01 +0900

    [MOD] text editer 미사용 주석

--- client/src/components/service/editableText/EditableTextTinymce.tsx
+++ client/src/components/service/editableText/EditableTextTinymce.tsx
@@ -1,39 +1,39 @@
-import React, { useRef, useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import styled from "@emotion/styled";
+// import React, { useRef, useState } from "react";
+// import { Editor } from "@tinymce/tinymce-react";
+// import styled from "@emotion/styled";
 
-const EditorContainer = styled.div`
-  position: relative;
-  display: inline-block;
-`;
+// const EditorContainer = styled.div`
+//   position: relative;
+//   display: inline-block;
+// `;
 
-interface TextEditorProps {
-  initialValue: string;
-  onChange: (content: string) => void;
-}
+// interface TextEditorProps {
+//   initialValue: string;
+//   onChange: (content: string) => void;
+// }
 
-const TextEditor: React.FC<TextEditorProps> = ({ initialValue, onChange }) => {
-  const editorRef = useRef(null);
+// const TextEditor: React.FC<TextEditorProps> = ({ initialValue, onChange }) => {
+//   const editorRef = useRef(null);
 
-  const handleEditorChange = (content: string) => {
-    onChange(content);
-  };
+//   const handleEditorChange = (content: string) => {
+//     onChange(content);
+//   };
 
-  return (
-    <EditorContainer>
-      <Editor
-        apiKey="your-api-key" // Replace with your TinyMCE API key
-        initialValue={initialValue}
-        inline={true}
-        init={{
-          menubar: false,
-          toolbar:
-            "fontselect fontsizeselect | bold italic underline strikethrough | forecolor | alignleft aligncenter alignright",
-        }}
-        onEditorChange={handleEditorChange}
-      />
-    </EditorContainer>
-  );
-};
+//   return (
+//     <EditorContainer>
+//       <Editor
+//         apiKey="your-api-key" // Replace with your TinyMCE API key
+//         initialValue={initialValue}
+//         inline={true}
+//         init={{
+//           menubar: false,
+//           toolbar:
+//             "fontselect fontsizeselect | bold italic underline strikethrough | forecolor | alignleft aligncenter alignright",
+//         }}
+//         onEditorChange={handleEditorChange}
+//       />
+//     </EditorContainer>
+//   );
+// };
 
-export default TextEditor;
+// export default TextEditor;
