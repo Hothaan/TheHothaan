c7a781fb70f673abd3fa3002c2b5ca3c4e6c6e36
song2 <hhhsi92@naver.com>
2024-11-05 10:55:22 +0900

    [MOD]cors오류수정

--- server/index.js
+++ server/index.js
@@ -12,7 +12,18 @@ const userRoutes = require("./routes/user");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use(express.json()); // JSON 요청을 처리하기 위한 미들웨어
+// CORS 설정
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://dolllpitoxic3.mycafe24.com"], // 허용할 도메인 목록 추가
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true, // 인증 정보(쿠키, 헤더 등)를 포함한 요청 허용
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// JSON 요청을 처리하기 위한 미들웨어
+app.use(express.json());
 
 // 엔드포인트 경로 설정
 app.use("/api/openai", openAIRoutes);
@@ -20,15 +31,6 @@ app.use("/api/user", userRoutes);
 
 // Swagger 문서화
 setupSwagger(app); // Swagger 설정 추가
-
-// CORS 설정
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://dolllpitoxic3.mycafe24.com"], // 허용할 도메인 목록 추가
-    methods: "GET,POST", // 허용할 HTTP 메소드 설정
-    credentials: true, // 인증 정보(쿠키, 헤더 등)를 포함한 요청 허용
-  })
-);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
