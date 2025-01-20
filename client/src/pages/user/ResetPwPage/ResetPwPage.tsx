36e50d5ab6fb4d3565d1bb5aaeaaa594edb6fb63
imlinkseo <imlinkseo@gmail.com>
2025-01-20 10:44:24 +0900

    [MOD] 빌드 테스트

--- client/src/pages/user/ResetPwPage/ResetPwPage.tsx
+++ client/src/pages/user/ResetPwPage/ResetPwPage.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 import { useState, useEffect } from "react";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import UserPageWrap from "@components/user/ui/UserPageWrap";
-import { ReactComponent as Lock } from "@svgs/findPwPage/lock.svg";
+// import { ReactComponent as Lock } from "@svgs/findPwPage/lock.svg";
 import TextField from "@components/common/form/TextField";
 import UserPwTextField, {
   IuserPwTextField,
@@ -114,7 +114,20 @@ export default function ResetPwPage() {
     <UserPageWrap>
       <div css={container}>
         <div css={information_container}>
-          <Lock />
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="50" height="50" rx="25" fill="#F6F8FF" />
+            <path
+              d="M25 15C22.243 15 20 17.243 20 20V23H19C18.4696 23 17.9609 23.2107 17.5858 23.5858C17.2107 23.9609 17 24.4696 17 25V33C17 33.5304 17.2107 34.0391 17.5858 34.4142C17.9609 34.7893 18.4696 35 19 35H31C31.5304 35 32.0391 34.7893 32.4142 34.4142C32.7893 34.0391 33 33.5304 33 33V25C33 24.4696 32.7893 23.9609 32.4142 23.5858C32.0391 23.2107 31.5304 23 31 23H30V20C30 17.243 27.757 15 25 15ZM22 20C22 18.346 23.346 17 25 17C26.654 17 28 18.346 28 20V23H22V20ZM26 30.723V33H24V30.723C23.6504 30.5228 23.3697 30.2213 23.1948 29.8584C23.02 29.4954 22.9593 29.0879 23.0207 28.6898C23.0821 28.2916 23.2627 27.9214 23.5388 27.6279C23.8148 27.3345 24.1733 27.1316 24.567 27.046C24.8594 26.9813 25.1626 26.9832 25.4542 27.0513C25.7459 27.1194 26.0185 27.2521 26.252 27.4397C26.4855 27.6272 26.6739 27.8647 26.8034 28.1348C26.9328 28.4049 27 28.7005 27 29C26.9994 29.3497 26.9067 29.6932 26.7311 29.9956C26.5556 30.2981 26.3034 30.549 26 30.723Z"
+              fill="#119CD4"
+            />
+          </svg>
+
           <p css={title}>비밀번호 재설정</p>
           <p css={desc}>새로 사용하기를 원하는 비밀번호를 입력해주세요.</p>
         </div>
