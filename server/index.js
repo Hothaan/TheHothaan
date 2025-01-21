6a9601538a7ebd49286b2385988ab7a67a9b3b2e
osw0124 <osw0124@daum.net>
2025-01-21 11:57:54 +0900

    [FIX] Update server to listen on all network interfaces

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
 // 환경 설정 로드
-require("dotenv").config({
-  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
 });
 
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-const setupSwagger = require("./swagger");
+const setupSwagger = require('./swagger');
 
-const openAIRoutes = require("./routes/openAI");
-const userRoutes = require("./routes/user");
-const imageRoutes = require("./routes/image");
-const serviceRoutes = require("./routes/service");
-const projectRoutes = require("./routes/project");
+const openAIRoutes = require('./routes/openAI');
+const userRoutes = require('./routes/user');
+const imageRoutes = require('./routes/image');
+const serviceRoutes = require('./routes/service');
+const projectRoutes = require('./routes/project');
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://dolllpitoxic3.mycafe24.com"], // 프론트 이슈로 임시 사용
+    origin: ['http://localhost:3000', 'http://dolllpitoxic3.mycafe24.com'], // 프론트 이슈로 임시 사용
     // origin: process.env.CORS_ORIGIN,
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     credentials: true, // 인증 정보(쿠키, 헤더 등)를 포함한 요청 허용
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
 );
 
 // 캐시 방지 헤더 설정 (API 요청에만 적용)
-app.use("/api", (req, res, next) => {
-  res.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-  res.set("Pragma", "no-cache");
-  res.set("Expires", "0");
+app.use('/api', (req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
   next();
 });
 
@@ -40,11 +40,11 @@ app.use("/api", (req, res, next) => {
 app.use(express.json());
 
 // 엔드포인트 경로 설정
-app.use("/api/openai", openAIRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/image", imageRoutes);
-app.use("/api/service", serviceRoutes);
-app.use("/api/project", projectRoutes);
+app.use('/api/openai', openAIRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/image', imageRoutes);
+app.use('/api/service', serviceRoutes);
+app.use('/api/project', projectRoutes);
 
 // /var/www/images 경로를 "/images" URL로 접근할 수 있도록 설정
 app.use('/images', express.static(path.join('/var/www/images')));
@@ -52,14 +52,14 @@ app.use('/images', express.static(path.join('/var/www/images')));
 // Swagger 문서화
 setupSwagger(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
