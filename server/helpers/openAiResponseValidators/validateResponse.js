6fc8a781ff409f466d9c464cf664d0e9acf731f3
osw0124 <osw0124@daum.net>
2025-01-21 12:34:39 +0900

    [FIX] import 경로 오류 수정

--- server/helpers/openAiResponseValidators/validateResponse.js
+++ server/helpers/openAiResponseValidators/validateResponse.js
@@ -1,11 +1,12 @@
 const validateValidateCommunitySns = require('./communitySns/validateCommunitySns');
-const validateForIntermediaryMatch = require('./IntermediaryMatch/validateForIntermediaryMatch');
+const validateForIntermediaryMatch = require('./intermediaryMatch/validateForIntermediaryMatch');
 const validateForShoppingMall = require('./shoppingMall/validateForShoppipngMall');
 
-exports.validateResponse = function validateResponse(response, service, depth2, cnt) {
+function validateResponse(response, service, depth2, cnt) {
   if (!response.menu || !response.feature || !response.content) return false;
   if (service === '쇼핑몰') return validateForShoppingMall(response, depth2, cnt);
   if (service === '중개·매칭') return validateForIntermediaryMatch(response, depth2, cnt);
   if (service === '커뮤니티·sns') return validateValidateCommunitySns(response, depth2, cnt);
   return false;
-};
+}
+module.exports = validateResponse;
--- server/helpers/prompts/generatePrompt.js
+++ server/helpers/prompts/generatePrompt.js
@@ -2,9 +2,10 @@ const generateCommunitySnsPrompts = require('./communitySns');
 const generateIntermediaryMatchPrompt = require('./IntermediaryMatch');
 const generateShoppingMallPrompt = require('./shoppingMall');
 
-exports.generatePrompt = function generatePrompt(serviceDetails) {
+function generatePrompt(serviceDetails) {
   const { service } = serviceDetails;
   if (service === '쇼핑몰') return generateShoppingMallPrompt(serviceDetails);
   if (service === '중개·매칭') return generateIntermediaryMatchPrompt(serviceDetails);
   if (service === '커뮤니티·sns') return generateCommunitySnsPrompts(serviceDetails);
-};
+}
+module.exports = generatePrompt;
