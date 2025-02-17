734873e9c7c8c9e0e4de80676de201437912dd38
osw0124 <osw0124@daum.net>
2025-02-17 14:10:25 +0900

    [MOD] generateOpenAiText()  모델 gpt-4o-mini로 변경경

--- server/helpers/openAiHelper.js
+++ server/helpers/openAiHelper.js
@@ -103,7 +103,7 @@ async function generateOpenAiText(
     const response = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: 'gpt-4',
+        model: 'gpt-4o-mini',
         messages: [
           {
             role: 'system',
