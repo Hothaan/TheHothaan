7c83979de6307d52e7d497debd32c0dc70a02708
osw0124 <osw0124@daum.net>
2025-02-21 14:52:33 +0900

    refactor: simplify product list validation logic for improved readability

--- server/helpers/openAiResponseValidators/shoppingMall/productListValidator.js
+++ server/helpers/openAiResponseValidators/shoppingMall/productListValidator.js
@@ -4,21 +4,8 @@ function productListValidator(response, contentLength) {
       response.content?.productListCategories &&
       response.content?.productListProductTitle &&
       response.content?.productListProductDesc
-    ) {
-      if (
-        Array.isArray(response.content.productListCategories) &&
-        Array.isArray(response.content.productListProductTitle) &&
-        Array.isArray(response.content.productListProductDesc)
-      ) {
-        if (
-          response.content.productListCategories.length === 6 &&
-          response.content.productListProductTitle.length === 15 &&
-          response.content.productListProductDesc.length === 15
-        ) {
-          return true;
-        }
-      }
-    }
+    )
+      return true;
   }
   return false;
 }
