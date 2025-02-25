0c83b141f109a691f52d1982b95299f1b2330e87
osw0124 <osw0124@daum.net>
2025-02-25 23:05:24 +0900

    [MOD] Update brandIntroduceValidator to validate multiple item titles and descriptions

--- server/helpers/openAiResponseValidators/shoppingMall/brandIntroduceValidator.js
+++ server/helpers/openAiResponseValidators/shoppingMall/brandIntroduceValidator.js
@@ -2,8 +2,11 @@ function validateBrandIntroduce(response) {
   if (!response.content) return false;
   if (!response.content.brandIntroduceBannerTitle) return false;
   if (!response.content.brandIntroduceBannerDesc) return false;
-  if (!response.content.brandIntroduceItemTitle) return false;
-  if (!response.content.brandIntroduceItemDesc) return false;
+  if (!response.content.brandIntroduceItemTitle1) return false;
+  if (!response.content.brandIntroduceItemDesc1) return false;
+  if (!response.content.brandIntroduceItemTitle2) return false;
+  if (!response.content.brandIntroduceItemDesc2) return false;
+
   return true;
 }
 module.exports = validateBrandIntroduce;
