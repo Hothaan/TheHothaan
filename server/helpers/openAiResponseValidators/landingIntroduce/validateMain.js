beb38ef040ac4eaac4f3323c72de81fc3579bfa4
osw0124 <osw0124@daum.net>
2025-02-26 00:18:42 +0900

    [MOD] Update validateMain to validate additional product image and description fields

--- server/helpers/openAiResponseValidators/landingIntroduce/validateMain.js
+++ server/helpers/openAiResponseValidators/landingIntroduce/validateMain.js
@@ -5,9 +5,12 @@ function validateMain(response) {
   if (!response.content.mainBannerButton) return false;
   if (!response.content.serviceIntroduceTitle) return false;
   if (!response.content.serviceIntroduceDesc) return false;
-  if (!response.content.productIntroduceImageDesc) return false;
-  if (!response.content.productIntroduceTitle) return false;
-  if (!response.content.productIntroduceDesc) return false;
+  if (!response.content.productIntroduceImageDesc1) return false;
+  if (!response.content.productIntroduceTitle1) return false;
+  if (!response.content.productIntroduceDesc1) return false;
+  if (!response.content.productIntroduceImageDesc2) return false;
+  if (!response.content.productIntroduceTitle2) return false;
+  if (!response.content.productIntroduceDesc2) return false;
   if (!response.content.noticeTitle) return false;
   if (!response.content.noticeDesc) return false;
   if (!response.content.recruitTitle) return false;
